c7f5447bdc43dc9c7549752d67442d37b2489e02
Jungmingyu <vlxjalsrb@naver.com>
2022-07-02 12:09:12 +0900

    2day

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) //읽기 모드 최적화, 데이터 변경이 안됨
+@RequiredArgsConstructor // final 이 붙은것만 생성자 생성. lombok 라이브러리로부터 import 가능
+public class MemberService {
+
+    private final MemberRepository memberRepository; //변경할 필요가 없으므로 final.
+
+    //회원 가입
+    @Transactional //읽고 쓰기 모두 가능한 transactional -> readonly =  false
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //중복회원이면 예외 터트림
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        }
+    }
+
+    //회원 전체 조회
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
